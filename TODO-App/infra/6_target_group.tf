c65d7bf6cce1863d1b83bf8dcc67746fb01b75ae
Mazen Lawand <malawand@cisco.com>
2021-05-05 11:23:28 -0500

    Fresh repo

--- TODO-App/infra/6_target_group.tf
+++
@@ -1,9 +0,0 @@
-# Create a target group for the ASG using aws_lbtarget_group
-resource "aws_lb_target_group" "asg" {
-    name        = "${var.environment}-${var.application}"
-    port        = var.server_port
-    protocol    = "HTTP"
-    vpc_id      = data.aws_vpc.default.id
-
-    # We need to reinstall todo and have it spin up faster on boot
-}
--- TODO-App/infra/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-    region = "${var.region}"
-}
-
-output "test" {
-    value = data.aws_subnet_ids.default
-}
